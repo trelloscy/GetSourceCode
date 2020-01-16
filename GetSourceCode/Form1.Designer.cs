a45f314d453d6ccd3ac27dd0072eda2e1646d1b9
Themos Kolios <KoliosT@dotnet.dotcy.com.cy>
2020-01-16 17:51:14 +0200

    Update repo from backed up solution

--- GetSourceCode/Form1.Designer.cs
+++ GetSourceCode/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -41,33 +41,44 @@
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSearch
             // 
-            this.button1.Location = new System.Drawing.Point(129, 65);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 22);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSearch.Location = new System.Drawing.Point(125, 63);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(61, 22);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 174);
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 164);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(831, 340);
+            this.richTextBox1.Size = new System.Drawing.Size(994, 402);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 114);
+            this.label1.Location = new System.Drawing.Point(2, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
@@ -75,7 +86,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 131);
+            this.textBox1.Location = new System.Drawing.Point(5, 129);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(114, 20);
@@ -83,7 +94,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(129, 131);
+            this.textBox2.Location = new System.Drawing.Point(125, 129);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(114, 20);
@@ -92,7 +103,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(126, 114);
+            this.label2.Location = new System.Drawing.Point(122, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 13);
             this.label2.TabIndex = 4;
@@ -100,7 +111,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(249, 131);
+            this.textBox3.Location = new System.Drawing.Point(245, 129);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(114, 20);
@@ -109,7 +120,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(246, 114);
+            this.label3.Location = new System.Drawing.Point(242, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 13);
             this.label3.TabIndex = 6;
@@ -117,7 +128,7 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(9, 66);
+            this.txtUsername.Location = new System.Drawing.Point(5, 64);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(114, 20);
             this.txtUsername.TabIndex = 8;
@@ -125,7 +136,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(369, 131);
+            this.textBox4.Location = new System.Drawing.Point(365, 129);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(114, 20);
@@ -134,7 +145,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(366, 114);
+            this.label4.Location = new System.Drawing.Point(362, 112);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 9;
@@ -142,7 +153,8 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(669, 12);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(847, 8);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.TabIndex = 11;
@@ -151,47 +163,115 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::GetSourceCode.Images.instagram_logo;
-            this.pictureBox2.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(5, 10);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(181, 42);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1013, 595);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.btnSearch);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtUsername);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1005, 569);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Instagram";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Controls.Add(this.btnParse);
+            this.tabPage2.Controls.Add(this.progressBar1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1005, 569);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "LinkedIn";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(3, 54);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(999, 473);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // btnParse
+            // 
+            this.btnParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnParse.Location = new System.Drawing.Point(844, 26);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(155, 22);
+            this.btnParse.TabIndex = 1;
+            this.btnParse.Text = "Paste and Parse Data";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(3, 543);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(999, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
             // Form1
             // 
-            this.AcceptButton = this.button1;
+            this.AcceptButton = this.btnSearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 514);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1013, 595);
+            this.Controls.Add(this.tabControl1);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Instagram Scraper";
+            this.Text = "Get Source Code";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
@@ -204,6 +284,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- GetSourceCode/Form1.cs
+++ GetSourceCode/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,7 +22,7 @@ namespace GetSourceCode
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnSearch_Click(object sender, EventArgs e)
         {
             try
             {
@@ -55,10 +57,30 @@ namespace GetSourceCode
             var match = Regex.Match(richTextBox1.Text, pattern);
             if (match.Success && match.Groups.Count > 1)
             {
-                result = match.Groups[1].Value;
+                //result = match.Groups[1].Value.Replace("\\u0026", "&");
+                result = Regex.Unescape(match.Groups[1].Value);
             }
 
             return result;
+        }
+        private Task<string> RegexCheckAll(string source, string pattern)
+        {
+            string result = "";
+
+            return Task.Run(() =>
+            {
+                var matches = Regex.Matches(source, pattern);
+                foreach (Match match in matches)
+                {
+                    var text = match.Groups[1].Value;
+                    text = "{" + text.Replace("&#92;\"", "'") + "}";
+                    JToken parsedJson = JToken.Parse(text);
+                    var beautified = parsedJson.ToString(Formatting.Indented);
+                    result += beautified + Environment.NewLine;
+                }
+
+                return result;
+            });
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -66,6 +88,51 @@ namespace GetSourceCode
             if (e.KeyCode == Keys.Escape)
             {
                 Close();
+            }
+        }
+
+        private async void btnParse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox2.Text = Clipboard.GetText();
+
+                //String url = "https://www.linkedin.com/me/profile-views/urn:li:wvmp:summary/";
+                //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+                //{
+                //    richTextBox2.Text = await sr.ReadToEndAsync();
+                //}
+
+                //textBox1.Text = RegexCheck("\"full_name\":\"(.*?)\",");
+                //textBox2.Text = RegexCheck("\"edge_followed_by\":{\"count\":(\\d*)}");
+                //textBox3.Text = RegexCheck("\"edge_follow\":{\"count\":(\\d*)}");
+                //textBox4.Text = RegexCheck("\"edge_owner_to_timeline_media\":{\"count\":(\\d*),");
+                //pictureBox1.Load(RegexCheck("\"profile_pic_url\":\"(.*?)\""));
+
+                //System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+                //path.AddEllipse(0, 0, pictureBox1.Height, pictureBox1.Height);
+                //pictureBox1.Region = new Region(path);
+
+                if (string.IsNullOrEmpty(richTextBox2.Text))
+                {
+                    MessageBox.Show("No source code specified");
+                    return;
+                }
+
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                string sourceCode = richTextBox2.Text.Replace("&quot;", "\"");
+                //richTextBox2.Text = await RegexCheckAll(sourceCode, "({\"data\":.*})");
+                var result = await RegexCheckAll(sourceCode, "(\"firstName\":\".*?),\"objectUrn\"");
+                await Task.Delay(1500);
+                richTextBox2.Text = result;
+                //richTextBox2.ScrollToCaret();
+                progressBar1.Style = ProgressBarStyle.Blocks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
