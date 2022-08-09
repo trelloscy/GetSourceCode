95995424fe214d5054e6276952cca08268a87127
themoscy <t.kolios@exeliatech.com>
2022-08-09 16:12:03 +0300

    Update project to VS2022. File Cleanup.

--- GetSourceCode/Form1.Designer.cs
+++ GetSourceCode/Form1.Designer.cs
@@ -64,10 +64,9 @@
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(167, 78);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Location = new System.Drawing.Point(125, 63);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(81, 27);
+            this.btnSearch.Size = new System.Drawing.Size(61, 22);
             this.btnSearch.TabIndex = 0;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -78,115 +77,103 @@
             this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(4, 202);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 164);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1324, 494);
+            this.richTextBox1.Size = new System.Drawing.Size(994, 402);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 138);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(2, 112);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Full name:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 159);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(5, 129);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(167, 159);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Location = new System.Drawing.Point(125, 129);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(151, 22);
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
             this.textBox2.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(163, 138);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(122, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.Size = new System.Drawing.Size(84, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Followers count:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(327, 159);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Location = new System.Drawing.Point(245, 129);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(151, 22);
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
             this.textBox3.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(323, 138);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(242, 112);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 17);
+            this.label3.Size = new System.Drawing.Size(84, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Following count:";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(7, 79);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Location = new System.Drawing.Point(5, 64);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(151, 22);
+            this.txtUsername.Size = new System.Drawing.Size(114, 20);
             this.txtUsername.TabIndex = 8;
-            this.txtUsername.Text = "themos.eats";
+            this.txtUsername.Text = "themos.eatscy";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(487, 159);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Location = new System.Drawing.Point(365, 129);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(151, 22);
+            this.textBox4.Size = new System.Drawing.Size(114, 20);
             this.textBox4.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(483, 138);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(362, 112);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.Size = new System.Drawing.Size(61, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Post count:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(1129, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(847, 8);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 185);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::GetSourceCode.Images.instagram_logo;
-            this.pictureBox2.Location = new System.Drawing.Point(7, 12);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(5, 10);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(241, 52);
+            this.pictureBox2.Size = new System.Drawing.Size(181, 42);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
@@ -198,10 +185,9 @@
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1351, 732);
+            this.tabControl1.Size = new System.Drawing.Size(1013, 595);
             this.tabControl1.TabIndex = 13;
             // 
             // tabPage1
@@ -222,30 +208,27 @@
             this.tabPage1.Controls.Add(this.txtUsername);
             this.tabPage1.Controls.Add(this.textBox2);
             this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1343, 703);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Size = new System.Drawing.Size(1005, 569);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Instagram";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(646, 159);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Location = new System.Drawing.Point(484, 129);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(151, 22);
+            this.textBox5.Size = new System.Drawing.Size(114, 20);
             this.textBox5.TabIndex = 14;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(642, 138);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(482, 112);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 17);
+            this.label5.Size = new System.Drawing.Size(80, 13);
             this.label5.TabIndex = 13;
             this.label5.Text = "Last Post Date:";
             // 
@@ -255,11 +238,10 @@
             this.tabPage2.Controls.Add(this.richTextBox2);
             this.tabPage2.Controls.Add(this.btnParse);
             this.tabPage2.Controls.Add(this.progressBar1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1343, 703);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Size = new System.Drawing.Size(1005, 569);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "LinkedIn";
             // 
@@ -268,20 +250,18 @@
             this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox2.Location = new System.Drawing.Point(4, 66);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox2.Location = new System.Drawing.Point(3, 54);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(1331, 581);
+            this.richTextBox2.Size = new System.Drawing.Size(999, 473);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             // 
             // btnParse
             // 
             this.btnParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnParse.Location = new System.Drawing.Point(1125, 32);
-            this.btnParse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnParse.Location = new System.Drawing.Point(844, 26);
             this.btnParse.Name = "btnParse";
-            this.btnParse.Size = new System.Drawing.Size(207, 27);
+            this.btnParse.Size = new System.Drawing.Size(155, 22);
             this.btnParse.TabIndex = 1;
             this.btnParse.Text = "Paste and Parse Data";
             this.btnParse.UseVisualStyleBackColor = true;
@@ -290,10 +270,9 @@
             // progressBar1
             // 
             this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.progressBar1.Location = new System.Drawing.Point(4, 671);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Location = new System.Drawing.Point(3, 543);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1335, 28);
+            this.progressBar1.Size = new System.Drawing.Size(999, 23);
             this.progressBar1.TabIndex = 4;
             // 
             // tabPage3
@@ -303,18 +282,20 @@
             this.tabPage3.Controls.Add(this.lblCounter);
             this.tabPage3.Controls.Add(this.btnParseFollowers);
             this.tabPage3.Controls.Add(this.txtFollowersSource);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1343, 703);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Size = new System.Drawing.Size(1005, 569);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "IG Followers";
             // 
             // btnOpenCSV
             // 
-            this.btnOpenCSV.Location = new System.Drawing.Point(504, 133);
+            this.btnOpenCSV.Location = new System.Drawing.Point(378, 108);
+            this.btnOpenCSV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOpenCSV.Name = "btnOpenCSV";
-            this.btnOpenCSV.Size = new System.Drawing.Size(163, 30);
+            this.btnOpenCSV.Size = new System.Drawing.Size(122, 24);
             this.btnOpenCSV.TabIndex = 3;
             this.btnOpenCSV.Text = "See results";
             this.btnOpenCSV.UseVisualStyleBackColor = true;
@@ -324,17 +305,19 @@
             // lblCounter
             // 
             this.lblCounter.AutoSize = true;
-            this.lblCounter.Location = new System.Drawing.Point(501, 85);
+            this.lblCounter.Location = new System.Drawing.Point(376, 69);
+            this.lblCounter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCounter.Name = "lblCounter";
-            this.lblCounter.Size = new System.Drawing.Size(103, 17);
+            this.lblCounter.Size = new System.Drawing.Size(80, 13);
             this.lblCounter.TabIndex = 2;
             this.lblCounter.Text = "Completed: 0/0";
             // 
             // btnParseFollowers
             // 
-            this.btnParseFollowers.Location = new System.Drawing.Point(504, 20);
+            this.btnParseFollowers.Location = new System.Drawing.Point(378, 16);
+            this.btnParseFollowers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnParseFollowers.Name = "btnParseFollowers";
-            this.btnParseFollowers.Size = new System.Drawing.Size(163, 52);
+            this.btnParseFollowers.Size = new System.Drawing.Size(122, 42);
             this.btnParseFollowers.TabIndex = 1;
             this.btnParseFollowers.Text = "Parse Followers";
             this.btnParseFollowers.UseVisualStyleBackColor = true;
@@ -342,23 +325,23 @@
             // 
             // txtFollowersSource
             // 
-            this.txtFollowersSource.Location = new System.Drawing.Point(24, 20);
+            this.txtFollowersSource.Location = new System.Drawing.Point(18, 16);
+            this.txtFollowersSource.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFollowersSource.MaxLength = 0;
             this.txtFollowersSource.Multiline = true;
             this.txtFollowersSource.Name = "txtFollowersSource";
             this.txtFollowersSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtFollowersSource.Size = new System.Drawing.Size(447, 628);
+            this.txtFollowersSource.Size = new System.Drawing.Size(336, 511);
             this.txtFollowersSource.TabIndex = 0;
             // 
             // Form1
             // 
             this.AcceptButton = this.btnSearch;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1351, 732);
+            this.ClientSize = new System.Drawing.Size(1013, 595);
             this.Controls.Add(this.tabControl1);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Get Source Code";
